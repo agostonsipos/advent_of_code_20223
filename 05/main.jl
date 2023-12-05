cc58095a87db8234aff8881cb765e2875cc0207c
Ágoston Sipos <agoston.sipos.95@gmail.com>
2023-12-05 09:50:42 +0100

    day 5

---
+++ 05/main.jl
@@ -0,0 +1,82 @@
+using DelimitedFiles
+
+t = readdlm("5.txt", '\n')
+
+function preprocess()
+    l = t[1]
+    parseint = x->parse(Int, x)
+    start = map(parseint, split(strip(split(t[1], ':')[2]), ' '))
+    transforms = []
+    for l in t[2:end]
+        if isempty(l)
+            continue
+        elseif occursin(":", l)
+            push!(transforms, [])
+        else
+            push!(transforms[end], map(parseint, split(l, ' ')))
+        end
+    end
+    return (start, transforms)
+end
+
+(start, transforms) = preprocess()
+
+
+function simulate(start, transforms)
+    data = copy(start)
+    for tr in transforms
+        for i in 1:length(data)
+            for range in tr
+                if range[2] <= data[i] < range[2] + range[3]
+                    data[i] += range[1] - range[2]
+                    break
+                end
+            end
+        end
+    end
+    return minimum(data)
+end
+
+println(simulate(start, transforms))
+
+
+function simulate2(start, transforms)
+    data = copy(start)
+    for tr in transforms
+        newdata = []
+        for i in 1:(length(data)÷2)
+            n = data[2*i-1]
+            l = data[2*i]
+            j = 1
+            while j <= length(tr)
+                range = tr[j]
+                if range[2] <= n < range[2] + range[3]
+                    if n + l < range[2] + range[3]
+                        push!(newdata, n + range[1] - range[2])
+                        push!(newdata, l)
+                        l = 0
+                        break
+                    else
+                        rangelen = range[2] + range[3] - n
+                        push!(newdata, n + range[1] - range[2])
+                        push!(newdata, rangelen)
+                        n += rangelen
+                        l -= rangelen
+                        j = 0
+                    end
+                end
+                j += 1
+            end
+        end
+        data = newdata
+    end
+    m = data[1]
+    for i in 1:length(data)÷2
+        if m > data[2*i-1]
+            m = data[2*i-1]
+        end
+    end
+    return m
+end
+
+println(simulate2(start, transforms))
